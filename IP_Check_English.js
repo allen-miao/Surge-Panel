7056acae380d7bb77102ff37316c4ef74c1ec50a
Allen <74881807+allen-miao@users.noreply.github.com>
2022-02-11 19:05:13 +0800

    Update IP_Check_English.js

--- IP_Check_English.js
+++ IP_Check_English.js
@@ -8,7 +8,7 @@ $httpClient.get(url, function(error, response, data){
     let isp = jsonData.isp
   body = {
     title: "Node information",
-    content: `IP address: {country} - ${city}\nOperator: ${isp}`,
+    content: `IP address: ${country} - ${city}\nOperator: ${isp}`,
     icon: "globe.asia.australia.fill"
   }
   $done(body);
