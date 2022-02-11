5d38ff66cc3c8bf52df80e6bad9da0faa724f71f
Allen <74881807+allen-miao@users.noreply.github.com>
2022-02-11 19:04:11 +0800

    Create IP_Check_English.js

---
+++ IP_Check_English.js
@@ -0,0 +1,24 @@
+let url = "http://ip-api.com/json/?lang=en-US"
+
+$httpClient.get(url, function(error, response, data){
+    let jsonData = JSON.parse(data)
+    let country = jsonData.country
+    let emoji = getFlagEmoji(jsonData.countryCode)
+    let city = jsonData.city
+    let isp = jsonData.isp
+  body = {
+    title: "Node information",
+    content: `IP address: {country} - ${city}\nOperator: ${isp}`,
+    icon: "globe.asia.australia.fill"
+  }
+  $done(body);
+});
+
+
+function getFlagEmoji(countryCode) {
+    const codePoints = countryCode
+      .toUpperCase()
+      .split('')
+      .map(char =>  127397 + char.charCodeAt());
+    return String.fromCodePoint(...codePoints);
+}
